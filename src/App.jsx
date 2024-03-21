9bfb3d2c1f0039d64724d735f0682496957955fd
Prashant Kumar <kumarprashant71037@gmail.com>
2024-03-22 04:24:34 +0530

    adding p tag

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <>
         <h1 className='text-red-900'>CarPool</h1>
         <h2 className='text-green-400'>Hello World!</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat pariatur unde saepe, molestiae ipsam repellendus! Ipsum doloremque blanditiis inventore quas eos eum ex necessitatibus.</p>
     </>
   )
 }
