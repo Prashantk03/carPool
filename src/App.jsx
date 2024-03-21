7c2667842acc00d9cbed21afd9c436b34906c017
gauravver13 <gauravver13@gmail.com>
2024-03-22 04:07:07 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1 className='text-red-900'>CarPool</h1>
+        <h2 className='text-green-400'>Hello World!</h2>
+    </>
+  )
+}
+
+export default App
